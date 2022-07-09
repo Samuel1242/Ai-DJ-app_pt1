bb1fe33d9e9d6ff1d5546ad4d0eb0e6dacda83f2
Samuel1242 <94230313+Samuel1242@users.noreply.github.com>
2022-07-09 11:20:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
+sound = "";
+
+function preLoad(){
+    sound = loadSound("music.mp3");
+}
+
 function setup(){
-    Video = createCapture(VIDEO)
-    Video.size(600,600)
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    Video = createCapture(VIDEO);
+    Video.hide();
+}
+
+function draw(){
+    image(Video, 0, 0,600,500);
+}
+
+function play(){
+
+    sound.play(); 
+
 }
