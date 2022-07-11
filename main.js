850121dc64995652c89e528e7e826356fcb78732
Samuel1242 <94230313+Samuel1242@users.noreply.github.com>
2022-07-11 11:22:58 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-sound = "";
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preLoad(){
-    sound = loadSound("music.mp3");
+    song = loadSound("music.mp3");
 }
 
 function setup(){
@@ -10,14 +14,38 @@ function setup(){
 
     Video = createCapture(VIDEO);
     Video.hide();
+
+    poseNet = ml5.poseNet(Video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+rightWristY);
+    
+    
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
 }
 
 function draw(){
     image(Video, 0, 0,600,500);
 }
 
-function play(){
-
-    sound.play(); 
-
+function play()
+{
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
 }
